f1d47f66fe4d59a7da198b4edc4bbeeb25682361
Nilay sharma <nilaysharma642@gmail.com>
2018-12-17 14:27:22 +0530

    Updates

---
+++ precache-manifest.356d42d90a5b122d36ea210906d40726.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "212264726e6b177ef5e7",
+    "url": "/shape-calculator/static/js/runtime~main.21226472.js"
+  },
+  {
+    "revision": "6d807f75ae572bdf1f5b",
+    "url": "/shape-calculator/static/js/main.6d807f75.chunk.js"
+  },
+  {
+    "revision": "2f7d112eafa86b212e46",
+    "url": "/shape-calculator/static/js/1.2f7d112e.chunk.js"
+  },
+  {
+    "revision": "6d807f75ae572bdf1f5b",
+    "url": "/shape-calculator/static/css/main.57ebc176.chunk.css"
+  },
+  {
+    "revision": "e6840981e9eb0060e241dee78ed085e0",
+    "url": "/shape-calculator/index.html"
+  }
+];
