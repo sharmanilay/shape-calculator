cbc4c738a6d27e7760ccb5e4ac72d885da302dea
Nilay sharma <nilaysharma642@gmail.com>
2018-12-20 11:58:52 +0530

    design changes

--- src/lorem.js
+++ src/lorem.js
@@ -12,16 +12,10 @@ export default class Lorem extends Component{
           Clicking cancel will take you back to step 1. Enjoy!
         </h4>
         <p>
-          A small river named Duden flows by their place and supplies it with the
-          necessary regelialia. It is a paradisematic country, in which roasted parts of
-          sentences fly into your mouth.
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
         <p>
-          Even the all-powerful Pointing has no control about the blind texts it is an
-          almost unorthographic life One day however a small line of blind text by the
-          name of Lorem Ipsum decided to leave for the far World of Grammar. The
-          Big Oxmox advised her not to do so, because there were thousands of bad
-          Commas.
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
         </p>
       </div>
     );
