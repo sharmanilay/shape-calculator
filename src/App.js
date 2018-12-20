c5f57f6242aab79247ff680e8d3cfaabc7df446f
Nilay sharma <nilaysharma642@gmail.com>
2018-12-20 10:57:22 +0530

    design changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './header.js';
 import MainContent from './main-content.js'
 import Footer from './footer.js'
-import './App.css';
+
 
 class App extends Component {
   render() {
